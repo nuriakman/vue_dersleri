a96ef6273c191e49d9a0fbb80f43dfbecfe2ca12
Nuri Akman <nuriakman@gmail.com>
2024-01-08 13:10:34 +0300

    Çalışmalara devam edildi

--- hat.kontrolu.php
+++ hat.kontrolu.php
@@ -3,31 +3,31 @@ header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, X-Requested-With");
 
-$jsonData = file_get_contents("php://input");
-$GelenData = json_decode($jsonData, true);
+$jsonDataString = file_get_contents("php://input");
+$GelenJsonData = json_decode($jsonDataString, true);
 
 $cevap = array();
-$cevap['TALEP'] = $GelenData;
-
-header('Content-Type: application/json; charset=utf-8');
-echo json_encode($cevap, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
-die();
-
-// POST verisini al
-$jsonData = file_get_contents("php://input");
-$data = json_decode($jsonData, true);
-
-
-// Eğer JSON verisi başarılı bir şekilde ayrıştırılırsa devam et
-if ($data !== null) {
-  // Burada gelen veriyi kullanabilirsiniz
-  // Örneğin, gelen veriyi ekrana yazdıralım:
-  $cevap['mesaj'] = "GELDİ";
+/*
+if ($GelenJsonData != null) {
+  http_response_code(400); // Hata oluştu
+  $cevap['hata'] = "json formatı yanlış!";
 } else {
-  // JSON ayrıştırma hatası
-  http_response_code(400); // Hatalı İstek
-  $cevap['mesaj'] = "Hatalı JSON verisi.";
+  */
+
+// gelen veri ile DB kontrol et
+if ($user == 0) {
+  // Login olmamış
+  http_response_code(401);
 }
+
+if ($yetki < 5) {
+  // Erişim Yetkisi Yok
+  http_response_code(403);
+}
+
+http_response_code(200); // İşlem başarılı
+$cevap['TALEP'] = $GelenJsonData;
+
 
 header('Content-Type: application/json; charset=utf-8');
 echo json_encode($cevap, JSON_PRETTY_PRINT | JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
--- vue-project/src/router/index.js
+++ vue-project/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import IletisimView from '../views/IletisimView.vue'
 import AdresSecView from '../views/AdresSecView.vue'
+import AboutView from '../views/AboutView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,7 +18,8 @@ const router = createRouter({
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      // component: () => import('../views/AboutView.vue')
+      component: AboutView
     },
     {
       path: '/iletisim',
