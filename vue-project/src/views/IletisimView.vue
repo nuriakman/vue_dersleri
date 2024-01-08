b1a0f72333777fc628ea0cad2028658ea56d60b5
Nuri Akman <nuriakman@gmail.com>
2024-01-08 14:40:44 +0300

    Vuetify için kullanım örneği eklendi

--- vue-project/src/views/IletisimView.vue
+++ vue-project/src/views/IletisimView.vue
@@ -1,6 +1,32 @@
+<script setup>
+import { ref, reactive, onMounted } from 'vue'
+const CEVAP = reactive({ iller: [], ilceler: [], semtler: [], mahalleler: [] })
+const SECIM = ref([])
+
+onMounted(() => {
+  getDataIller() // İlleri getir...
+  // this.IlSecildi();
+})
+function getDataIller() {
+  fetch('http://localhost/vue_dersleri/api.php?method=get.iller')
+    .then((response) => response.json())
+    .then((data) => (CEVAP.iller = data)) // DB'den gelen veriyi alalım...
+}
+</script>
+
 <template>
-  <main>
-    <h1>Selam, telefonlarımız burada...</h1>
-    Nuri: 123
-  </main>
+  <h1>Vuetify Kullanımı</h1>
+  <v-combobox
+    v-model="SECIM"
+    label="İl Seçiniz..."
+    :items="CEVAP.iller"
+    item-title="il_adi"
+    item-value="id"
+    variant="solo-filled"
+    clearable
+    multiple
+    chips
+  ></v-combobox>
+  <h1>Seçim:</h1>
+  <pre>{{ SECIM }}</pre>
 </template>
