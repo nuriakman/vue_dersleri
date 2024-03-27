da298520af6bee483511a44084e3a842162cf4e0
Nuri Akman <nuriakman@gmail.com>
2024-03-27 03:13:56 +0300

    Linkler güncellendi

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
-<p><a href='vue.html'>VUE Örneği</a></p>
+<p><a href='ornek.01.html'>VUE Örneği 01</a></p>
+<p><a href='ornek.02.html'>VUE Örneği 02</a></p>
+<p><a href='ornek.03.html'>VUE Örneği 03</a></p>
+<p><a href='ornek.04.html'>VUE Örneği 04</a></p>
 <p><a href='api.php?method=get.iller'>get.iller</a></p>
-<p><a href='api.php?method=get.ilceler&il_id=6'>get.ilceler?il_id=6</a></p>
+<p><a href='api.php?method=get.ilceler&il_id=6'>get.ilceler?id=6</a></p>
